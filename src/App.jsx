444c1c25569aa1be606e2635927928270862b28b
binumarcodes <abdulrahmanbinumar179@gmail.com>
2024-03-31 18:52:50 +0100

    Updated Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import Dashboard from './pages/Dashboard'
+import ReceiptPage from './pages/ReceiptPage'
+import Sidebar from './components/Sidebar'
 
 
 function App() {
@@ -10,20 +12,9 @@ function App() {
 
   return (
     <>
-
-      {/* <Router>
-        <Routes>
-          <Route path="/" exact element={<LoginPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/overview"  element={<OverviewSection />} />
-          <Route path="/receipts" element={<ReceiptSection />} />
-          <Route path="/allocation" element={<AllocationSection />} />
-          <Route path="/statistics" element={<StatisticsSection />} />
-          <Route path="/settings" element={<SettingsSection />} />
-        </Routes>
-      </Router> */}
-
+      {/* <Sidebar /> */}
       <Dashboard />
+      {/* <ReceiptPage /> */}
       
     </>
   )
