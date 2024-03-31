4e2a1ccfa8bcf6f70a2df3827f282fa24d92f61f
binumarcodes <abdulrahmanbinumar179@gmail.com>
2024-03-31 18:53:20 +0100

    Changed Section from sidebar to topbar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,79 +1,83 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import OverviewSection from '../sections/OverviewSection';
 import ReceiptSection from '../sections/ReceiptSection';
 import AllocationSection from '../sections/AllocationSection';
 import StatisticsSection from '../sections/StatisticsSection';
 import SettingsSection from '../sections/SettingsSection';
-import { AppBar, CssBaseline, Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography, Divider } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem } from '@mui/material';
 import { AccountCircle, Dashboard, Receipt, AssignmentTurnedIn, BarChart, Settings } from '@mui/icons-material';
-import { styled } from '@mui/system';
 
-const drawerWidth = 240;
-
-const DrawerHeader = styled('div')({
-  display: 'flex',
-  alignItems: 'center',
-  padding: '0 8px',
-  justifyContent: 'flex-end',
-});
-
-const MainContent = styled('div')({
-  marginLeft: drawerWidth,
-  padding: '24px',
-});
+const sections = [
+  { text: 'Overview', icon: <Dashboard />, path: '/', element: <OverviewSection /> },
+  { text: 'Receipt', icon: <Receipt />, path: '/receipts', element: <ReceiptSection /> },
+  { text: 'Allocation', icon: <AssignmentTurnedIn />, path: '/allocation', element: <AllocationSection /> },
+  { text: 'Statistics', icon: <BarChart />, path: '/statistics', element: <StatisticsSection /> },
+  { text: 'Settings', icon: <Settings />, path: '/settings', element: <SettingsSection /> },
+];
 
 const DashboardLayout = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Router>
       <div>
-        <CssBaseline />
         <AppBar position="fixed">
           <Toolbar>
-            <Typography variant="h6" noWrap style={{marginLeft: drawerWidth}}>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
               Dashboard
             </Typography>
-            <AccountCircle style={{ marginLeft: 'auto' }} />
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
           </Toolbar>
         </AppBar>
-        <Drawer
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            '& .MuiDrawer-paper': {
-              width: drawerWidth,
-            },
-          }}
-          variant="permanent"
-          anchor="left"
-        >
-          <DrawerHeader />
-          <Divider />
-          <List>
-            {[
-              { text: 'Overview', icon: <Dashboard />, path: '/' },
-              { text: 'Receipt', icon: <Receipt />, path: '/receipts' },
-              { text: 'Allocation', icon: <AssignmentTurnedIn />, path: '/allocation' },
-              { text: 'Statistics', icon: <BarChart />, path: '/statistics' },
-              { text: 'Settings', icon: <Settings />, path: '/settings' },
-            ].map((item, index) => (
-              <ListItem button key={index} component="a" href={item.path}>
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.text} />
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-        <MainContent>
-          <Toolbar />
-          <Routes>
-          <Route path="/" exact element={<OverviewSection />} />
-          <Route path="/receipts" element={<ReceiptSection />} />
-          <Route path="/allocation" element={<AllocationSection />} />
-          <Route path="/statistics" element={<StatisticsSection />} />
-          <Route path="/settings" element={<SettingsSection />} />
-          </Routes>
-        </MainContent>
+        <Toolbar /> {/* Add a spacer to push content below the app bar */}
+        <Toolbar>
+          {sections.map((section, index) => (
+            <IconButton key={index} color="inherit" component={Link} to={section.path}>
+              {section.icon}
+            </IconButton>
+          ))}
+        </Toolbar>
+        <Routes>
+          {sections.map((section, index) => (
+            <Route key={index} path={section.path} element={section.element} />
+          ))}
+        </Routes>
       </div>
     </Router>
   );
