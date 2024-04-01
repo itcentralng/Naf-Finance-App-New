e3a9f4d21c2e6c556bd3ecdf31d80eaf0cc4bbb4
binumarcodes <abdulrahmanbinumar179@gmail.com>
2024-04-01 10:34:57 +0100

    Make it as the General Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import Dashboard from './pages/Dashboard'
-import ReceiptPage from './pages/ReceiptPage'
-import Sidebar from './components/Sidebar'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from "./components/Sidebar"
+import OverviewSection from './sections/OverviewSection';
+import ReceiptSection from './sections/ReceiptSection';
+import AllocationSection from './sections/AllocationSection';
+import StatisticsSection from './sections/StatisticsSection';
+import SettingsSection from './sections/SettingsSection';
+import Dashboard from "./pages/Dashboard"
+import ReceiptPage from "./pages/ReceiptPage"
 
-
-function App() {
-
-  
-
+const App = () => {
   return (
-    <>
-      {/* <Sidebar /> */}
-      <Dashboard />
-      {/* <ReceiptPage /> */}
-      
-    </>
-  )
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Dashboard />}>
+            <Route index element={<OverviewSection />} />
+            <Route path="/receipts" element={<ReceiptSection />} />
+            <Route path="/allocation" element={<AllocationSection />} />
+            <Route path="/statistics" element={<StatisticsSection />} />
+            <Route path="/settings" element={<SettingsSection />} />
+            <Route path="/receiptpage" element={<ReceiptPage />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
