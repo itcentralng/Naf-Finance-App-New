f8480cf5d1797f43a0adc0cdf0c68d8ced97ef8b
binumarcodes <abdulrahmanbinumar179@gmail.com>
2024-04-01 10:35:55 +0100

    Updated Dashboard Section to topbar and updated the route

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Tooltip } from '@mui/material';
+import { AccountCircle, Dashboard, Receipt, AssignmentTurnedIn, BarChart, Settings } from '@mui/icons-material';
 import OverviewSection from '../sections/OverviewSection';
 import ReceiptSection from '../sections/ReceiptSection';
 import AllocationSection from '../sections/AllocationSection';
 import StatisticsSection from '../sections/StatisticsSection';
 import SettingsSection from '../sections/SettingsSection';
-import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem } from '@mui/material';
-import { AccountCircle, Dashboard, Receipt, AssignmentTurnedIn, BarChart, Settings } from '@mui/icons-material';
 
 const sections = [
-  { text: 'Overview', icon: <Dashboard />, path: '/', element: <OverviewSection /> },
-  { text: 'Receipt', icon: <Receipt />, path: '/receipts', element: <ReceiptSection /> },
-  { text: 'Allocation', icon: <AssignmentTurnedIn />, path: '/allocation', element: <AllocationSection /> },
-  { text: 'Statistics', icon: <BarChart />, path: '/statistics', element: <StatisticsSection /> },
-  { text: 'Settings', icon: <Settings />, path: '/settings', element: <SettingsSection /> },
+  { text: 'Overview', icon: <Dashboard />, path: '/' },
+  { text: 'Receipt', icon: <Receipt />, path: '/receipts' },
+  { text: 'Allocation', icon: <AssignmentTurnedIn />, path: '/allocation' },
+  { text: 'Statistics', icon: <BarChart />, path: '/statistics' },
+  { text: 'Settings', icon: <Settings />, path: '/settings' },
 ];
 
 const DashboardLayout = () => {
@@ -28,58 +28,59 @@ const DashboardLayout = () => {
   };
 
   return (
-    <Router>
-      <div>
-        <AppBar position="fixed">
-          <Toolbar>
-            <Typography variant="h6" sx={{ flexGrow: 1 }}>
-              Dashboard
-            </Typography>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>Profile</MenuItem>
-              <MenuItem onClick={handleClose}>My account</MenuItem>
-            </Menu>
-          </Toolbar>
-        </AppBar>
-        <Toolbar /> {/* Add a spacer to push content below the app bar */}
+    <div style={{ padding: 20}}>
+      <AppBar position="fixed">
         <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Dashboard
+          </Typography>
           {sections.map((section, index) => (
-            <IconButton key={index} color="inherit" component={Link} to={section.path}>
-              {section.icon}
-            </IconButton>
+            <Tooltip title={section.text} key={index}>
+              <IconButton color="inherit" component={Link} to={section.path}>
+                {section.icon}
+              </IconButton>
+            </Tooltip>
           ))}
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+          </Menu>
         </Toolbar>
-        <Routes>
-          {sections.map((section, index) => (
-            <Route key={index} path={section.path} element={section.element} />
-          ))}
-        </Routes>
-      </div>
-    </Router>
+      </AppBar>
+      <Toolbar /> {/* Add a spacer to push content below the app bar */}
+      <Toolbar />
+      <Routes>
+        <Route path="/" element={<OverviewSection />} />
+        <Route path="/receipts" element={<ReceiptSection />} />
+        <Route path="/allocation" element={<AllocationSection />} />
+        <Route path="/statistics" element={<StatisticsSection />} />
+        <Route path="/settings" element={<SettingsSection />} />
+      </Routes>
+    </div>
   );
 }
 
